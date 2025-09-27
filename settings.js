ff560f94636c36185c28123b5bcb43bfc159d48d
mrpotato0777 <mrpotato77769666@gmail.com>
2025-09-27 10:46:13 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: ' 🐝باتشيرا عمك',
+  author: 'bachira‎',
+  botName: "Bachira Bot",
+  botOwner: 'Bachira', // Your name
+  ownerNumber: '96103065822', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.2.0",
+  version: "2.0",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
